d91038a8e12a4ba9130535f7ad4a9d623bc86b9c
Katriaryna Mazgo <ekaterina.mozgo@mail.ru>
2018-09-19 16:20:18 +0300

    zeros/fix: implemented function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let zerosCount = 0;
+    let power = 1;
+
+    while (number / (Math.pow(5, power)) >= 1) {
+        let numbOfDivisors = Math.floor(number / (Math.pow(5, power)));
+        zerosCount += numbOfDivisors;
+        power ++;
+    }
+
+    return zerosCount;
 }
